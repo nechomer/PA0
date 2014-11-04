82d0e6f28fe5deb8bc699b97044b9de110e2e015
liorberc <liorberc1@gmail.com>
2014-11-04 19:48:46 +0200

    d

---
+++ BasicInterpreter/IfCmd.java
@@ -0,0 +1,53 @@
+package basicInterpreter;
+
+public class IfCmd extends Cmd {
+	
+	private char var1, var2;
+	private int boolOP;
+	private Cmd nextCmd;
+	
+	public char getVar1() {
+		return var1;
+	}
+
+	public void setVar1(char var1) {
+		this.var1 = var1;
+	}
+
+	public char getVar2() {
+		return var2;
+	}
+
+	public void setVar2(char var2) {
+		this.var2 = var2;
+	}
+
+	public int getBoolOP() {
+		return boolOP;
+	}
+
+	public void setBoolOP(int boolOP) {
+		this.boolOP = boolOP;
+	}
+
+	public Cmd getNextCmd() {
+		return nextCmd;
+	}
+
+	public void setNextCmd(Cmd nextCmd) {
+		this.nextCmd = nextCmd;
+	}
+	
+	public IfCmd(int currentLineNumber, Lexer lex, char var1, char var2, int boolOP, Cmd nextCmd) {
+		super(IF_CMD, currentLineNumber);
+		this.var1 = var1;
+		this.var2 = var2;
+		this.boolOP = boolOP;
+		this.nextCmd = nextCmd;
+		parseCMD(this, lex);
+	}
+	
+	private static void parceCMD(IfCmd ifCmd, ) {
+		
+	}
+}
