663c9743765caa33458c1f59c517a5b541b499f6
roeyasher <roey.asher@gmail.com>
2014-11-05 17:38:49 +0200

    update

--- BasicInterpreter/Lexer.java
+++ BasicInterpreter/Lexer.java
@@ -1,14 +1,150 @@
 package basicInterpreter;
 
+
 public class Lexer {
-
-	public Lexer() {
-		// TODO Auto-generated constructor stub
+	
+    int currentPos = 0;
+    int previousPos = 0;
+    char buffer[];
+	public Lexer(String data) {
+		
+		buffer = data.toCharArray();
+		currentPos = 0;
 	}
+	
+    /** Return true if  char is whitespace. */
+    static boolean isSpace(char c) {
+        return ((c == ' ') || (c == '\t'));
+    }   
+    
+    static boolean isDigit(char c) {
+        return ((c >= '0') && (c <= '9'));
+    }
+    
+    static boolean isLetter(char c) {
+        return ((c >= 'a') && (c <= 'z'));
+    }
+    
+    public void lastToken() {
+        if (currentPos != previousPos) {
+            currentPos = previousPos;
+        }
+    }
+    
+	public Token nextToken() {
+		
+		
+        if (currentPos >= buffer.length)
+            return new Token(Token.Eol);
 
-	public Token getNextToken() {
-		Token token;
-		return token;
+        previousPos = currentPos;
+        
+        if (isSpace(buffer[currentPos])) {
+        	currentPos++;
+        	return new Token(Token.Error);
+        }
+		
+        switch (buffer[currentPos]) {
+        
+	        case '+' :
+	            currentPos++;
+	            return new Token(Token.BinOp, "+", Parameters.ADD);
+	
+	        case '-' :
+	            currentPos++;
+	            return new Token(Token.BinOp, "-", Parameters.SUB);
+	
+	        case '*' :
+	            currentPos++;
+	            return new Token(Token.BinOp, "*", Parameters.MUL);
+	
+	        case '/' :
+	            currentPos++;
+	            return new Token(Token.BinOp, "\\", Parameters.DIV);
+	            
+            case '<' :
+                if (buffer[currentPos+1] == '=') {
+                    currentPos += 2;
+                    return new Token(Token.BoolOp, "<=", Parameters.LE);
+                }
+                currentPos++;
+                return new Token(Token.BoolOp, "<", Parameters.LT);
+
+            case '>' :
+                if (buffer[currentPos+1] == '=') {
+                    currentPos += 2;
+                    return new Token(Token.BoolOp, ">=", Parameters.GE);
+                } 
+                currentPos++;
+                return new Token(Token.BoolOp, ">", Parameters.GT);
+                
+            case ':' :
+                if (buffer[currentPos+1] == '=') {
+                    currentPos += 2;
+                    return new Token(Token.Cmd, ":=", Cmd.ASSIGN_CMD);
+                } 
+                else
+                	return new Token(Token.Error);
+            
+            case 'i' :
+                if (currentPos+2 <= buffer.length && buffer[currentPos+1] == 'f' && buffer[currentPos+2] == '(' ) {
+                    currentPos += 2;
+                    return new Token(Token.Cmd, "if", Cmd.IF_CMD);
+                }
+                
+            case 'g' :
+                if (currentPos+4 <= buffer.length && buffer[currentPos+1] == 'o' && buffer[currentPos+2] == 't' && buffer[currentPos+3] == 'o' && buffer[currentPos+4] == ' ') {
+                    currentPos += 4;
+                    return new Token(Token.Cmd, "goto", Cmd.GOTO_CMD);
+                } 
+               
+            case 'p' :
+                if (currentPos+5 <= buffer.length && buffer[currentPos+1] == 'r' && buffer[currentPos+2] == 'i' && buffer[currentPos+3] == 'n' && buffer[currentPos+4] == 't' && buffer[currentPos+5] == '(') {
+                    currentPos += 5;
+                    return new Token(Token.Cmd, "print", Cmd.PRINT_CMD);
+                } 
+                
+            case '(' :					
+            case ')' :
+            case ';' :
+                return new Token(Token.Symbol);   
+                
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            	
+            	int num = 0;
+            	while ( currentPos+1 <= buffer.length && isDigit(buffer[currentPos]) ) 
+            		num = (num*10) + (buffer[currentPos++] - '0');
+            	
+            	return new Token(Token.Num,num,0);
+            
+           default: break;
+        }
+        
+        if(isLetter(buffer[currentPos])) {
+        	
+        	StringBuffer sb = new StringBuffer();
+	        
+        	while (isLetter(buffer[currentPos])) {
+	            sb.append(buffer[currentPos]);
+	            currentPos++;
+	        }
+	        
+	        return new Token(Token.Var,sb.toString(),0);
+        }
+
+
+        
+        
+        return new Token(Token.Error);
 	}
 
 }
--- BasicInterpreter/Token.java
+++ BasicInterpreter/Token.java
@@ -1,20 +1,25 @@
+package basicInterpreter;
 
 public class Token {
 	
 	
     final static String names[] = {
-        "cmd", "exp", "var", "num", "boolop", "binop",
+        "cmd", "exp", "var", "num", "boolop", "binop", "symbol", "error", "eol",
     };
     
     /** The different types of token */
-	 final static int Cmd = 0;
-	 final static int Exp = 1;
-	 final static int Var = 2;
-	 final static int Num = 3;
+	 final static int Cmd    = 0;
+	 final static int Exp    = 1;
+	 final static int Var    = 2;
+	 final static int Num    = 3;
 	 final static int BoolOp = 4;
-	 final static int BinOp = 5;
+	 final static int BinOp  = 5;
+	 final static int Symbol = 6;
+	 final static int Error  = 7;
+	 final static int Eol    = 8;
 	 
 	 protected int     type;     // token's type
+	 protected int 	   parVal;
 	 protected String  strVal;
 	 protected double  numVal;     // numeric value
 	 
@@ -34,15 +39,16 @@ public class Token {
 	     type = this.type;
      }
 	 
-	 Token(int type, double numVal) {
+	 Token(int type, double numVal, int parVal) {
 	     type = this.type;
          numVal = this.numVal;
+         parVal = this.parVal;
      }
 	 
-	 Token(int type, String strVal, double numVal) {
+	 Token(int type, String strVal, int parVal) {
 	     type = this.type;
 	     strVal = this.strVal;
-         numVal = this.numVal;
+	     parVal = this.parVal;
      }
 	 
 	 
